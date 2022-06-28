8954180bf95f824d533199dbcc27ecdde74f6c1b
Visakh <visakhbobby47@gmail.com>
2022-06-28 19:30:02 +0530

    day 12

---
+++ 160. Intersection of Two Linked Lists.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        l1 = headA
+        l2 = headB
+        l3 = []
+        
+        while(l1):
+            l3.append(l1)
+            l1 = l1.next
+        
+        
+        while(l2):
+            if l2 in l3:
+                return l2
+            l2 = l2.next
+        
+        return None
+        
+        
